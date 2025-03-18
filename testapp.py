6c98dc8bc733a4981d53bc3082b56f12769b51d8
Nw91_ops <Nw91@mahurangi.school.nz>
2025-03-19 12:24:37 +1300

    Added to the database

--- testapp.py
+++ testapp.py
@@ -1,11 +1,33 @@
-from flask import Flask
+from flask import Flask, g
+import sqlite3
+
+DATABASE = 'database.db'
 
 app = Flask(__name__)
+
+DATABASE = '/path/to/database.db'
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
 
 @app.route('/')
 def home():
     #home page
-    return "hello sigma"
+    db = get_db()
+    cursor = db.cursor
+    sql = "SELECT * FROM bikes;"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    return str(results)
 
 if __name__ == "__main__":
     app.run(debug=True)
